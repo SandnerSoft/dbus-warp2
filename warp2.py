dbc05e866ee9bf916399f8b9f6096193f0a10bfc
Sandner, Denny <sandnersoft@googlemail.com>
2023-03-09 21:14:43 +0100

    Update warp2.py

--- warp2.py
+++ warp2.py
@@ -39,6 +39,17 @@ class DbusWarp2Service:
         '/Mode'
         ]
     
+        websocket.enableTrace(True)
+        ws = websocket.WebSocketApp("wss://192.168.1.179/ws",
+                                on_open=self.on_open,
+                                on_message=self.on_message,
+                                on_error=self.on_error,
+                                on_close=self.on_close)
+
+        ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+        rel.signal(2, rel.abort)  # Keyboard Interrupt
+        rel.dispatch()
+
         #get data from go-eCharger
         data = self._getFirmwareVersion()
         
@@ -80,6 +91,18 @@ class DbusWarp2Service:
         
         # add _signOfLife 'timer' to get feedback in log every 5minutes
         gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
+
+    def on_message(ws, message):
+        print(message)
+
+    def on_error(ws, error):
+        print(error)
+
+    def on_close(ws, close_status_code, close_msg):
+        print("### closed ###")
+
+    def on_open(ws):
+        print("Opened connection")
 
     def _getWarpFirmwareURL(self):
         config = self._getConfig()
