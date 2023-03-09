d5ed477798e73864a94b6bee05edf522ca894b24
Sandner, Denny <sandnersoft@googlemail.com>
2023-03-09 14:14:17 +0100

    initial

---
+++ warp2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import platform 
+import logging
+import logging.handlers
+import sys
+import os
+import sys
+if sys.version_info.major == 2:
+    import gobject
+else:
+    from gi.repository import GLib as gobject
+import sys
+import time
+import requests # for http GET
+import configparser # for config/ini file
+ 
+# our own packages from victron
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
+from vedbus import VeDbusService
+
+class DbusWarp2Service:
+
+
+
+
+
+
+    def _getConfig(self):
+        config = configparser.ConfigParser()
+        config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
+        return config;
